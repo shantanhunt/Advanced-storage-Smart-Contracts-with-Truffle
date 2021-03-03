90ac038fdaa27dcc4fa5e78280f10e3b47e15722
Shantanu Sontakke <shantanusontakke015@gmail.com>
2021-03-03 20:50:14 +0530

    Advanced Storage Tested

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# These are the Truffle Tests
+#Advanced Storage Smart Contract
+An array named ids was declared here. The contract has the functions add(), get(), length() and getAll() in order to modify and get the contents of the array.
 
+# These are the Truffle Tests
+This Solidity Smart constract tested using Truffle gives the following results
 ![](/Screenshots/test1.JPG)
 
 
--- test/mytest.js
+++ test/mytest.js
@@ -2,12 +2,37 @@
 var AdvancedStorage = artifacts.require("AdvancedStorage");
 
 contract('AdvancedStorage', (accounts) => {
-    it('Farmer should call the Contract', async () => {
-      const advancedStorage = await AdvancedStorage.deployed();
+    let advancedStorage = null;
+    before(async () => {
+      advancedStorage = await AdvancedStorage.deployed(); // We wrote this once instead of doing this each time
+    });
+
+    it('Should add an element to ids array', async () => {
       await advancedStorage.add(10);
-      const result = await advancedStorage.ids(0);
+      const result = await advancedStorage.ids(0); //You can access any variable of the contract using the dot operator
   
       assert(result.toNumber() === 10);
     });
+
+    it('Should get an element of the ids array', async () => {
+      await advancedStorage.add(20);
+      const result = await advancedStorage.get(1);
+  
+      assert(result.toNumber() === 20);
+    });
+
+    it('Should get the ids array', async () => {
+      const rawIds = await advancedStorage.getAll();
+      const ids = rawIds.map(id => id.toNumber());
+  
+      assert.deepEqual(ids, [10,20]);
+    });
+
+    it('Should get the length of the ids array', async () => {
+      const length = await advancedStorage.length();
+  
+      assert(length.toNumber() ===2);
+    });
+
   });
   
